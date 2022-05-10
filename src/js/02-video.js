2ec8d924344a1fc80e1a78362c27f2df02c13fc5
Andrii <krupaandrii@gmail.com>
2022-05-10 23:19:12 +0300

    task 2&3 fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -19,20 +19,7 @@ player.on('timeupdate', throttle(onPlay, 1000));
 
 let whenStopped = localStorage.getItem('videoplayer-current-time');
 // console.log(whenStopped);
-player
-  .setCurrentTime(whenStopped)
-  .then(function (seconds) {
-    // seconds = localStorage.getItem('videoplayer-current-time');
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
 
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+if (whenStopped) {
+  player.setCurrentTime(whenStopped);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,41 +2,102 @@ const throttle = require('lodash.throttle');
 
 const refs = {
   form: document.querySelector('.feedback-form'),
-  email: document.querySelector('.feedback-form input'),
-  message: document.querySelector('.feedback-form textarea'),
 };
 
-console.log(refs.email);
-console.log(refs.message);
+const STORAGE_KEY = 'feedback-form-state';
+
+// console.log(refs.email);
+// console.log(refs.message);
 console.log(refs.form);
 
 refs.form.addEventListener('submit', onFormSubmit);
 refs.form.addEventListener('input', throttle(onInputValues, 500));
 
-const inputValues = localStorage.getItem('feedback-form-state')
-  ? JSON.parse(localStorage.getItem('feedback-form-state'))
-  : {};
-
 onSavedInput();
 
 function onInputValues(e) {
-  inputValues[e.target.name] = e.target.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(inputValues));
+  const inputData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+
+  if (e.target.name === 'email') {
+    inputData.email = e.target.value;
+  } else if (e.target.name === 'message') {
+    inputData.message = e.target.value;
+  }
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(inputData));
 }
 
 function onFormSubmit(e) {
   e.preventDefault();
-  e.currentTarget.reset();
-  localStorage.removeItem('feedback-form-state');
+
+  const email = refs.form.elements.email.value;
+  const message = refs.form.elements.message.value;
+
+  if (!email || !message) {
+    return alert('Some field(s) still empty');
+  }
+
+  refs.form.reset();
+  localStorage.removeItem(STORAGE_KEY);
 }
 
 function onSavedInput(e) {
-  if (!localStorage.getItem('feedback-form-state')) {
-    return;
-  }
-  const savedInput = JSON.parse(localStorage.getItem('feedback-form-state'));
+  const savedInput = JSON.parse(localStorage.getItem(STORAGE_KEY));
   if (savedInput) {
-    refs.email.value = inputValues.email ? inputValues.email : '';
-    refs.message.value = inputValues.message ? inputValues.message : '';
+    refs.form.elements.email.value = savedInput.email || '';
+    refs.form.elements.message.value = savedInput.message || '';
   }
 }
+
+// =========== working code but rejected by the mentor =====
+
+// const throttle = require('lodash.throttle');
+
+// const refs = {
+//   form: document.querySelector('.feedback-form'),
+//   email: document.querySelector('.feedback-form input'),
+//   message: document.querySelector('.feedback-form textarea'),
+// };
+
+// const STORAGE_KEY = 'feedback-form-state';
+
+// console.log(refs.email);
+// console.log(refs.message);
+// console.log(refs.form);
+
+// refs.form.addEventListener('submit', onFormSubmit);
+// refs.form.addEventListener('input', throttle(onInputValues, 500));
+
+// const inputValues = localStorage.getItem(STORAGE_KEY)
+//   ? JSON.parse(localStorage.getItem(STORAGE_KEY))
+//   : {};
+
+// onSavedInput();
+
+// function onInputValues(e) {
+//   inputValues[e.target.name] = e.target.value;
+//   localStorage.setItem(STORAGE_KEY, JSON.stringify(inputValues));
+// }
+
+// function onFormSubmit(e) {
+//   e.preventDefault();
+//   if (!refs.email.value || !refs.message.value) {
+//     return alert('Some field(s) still empty');
+//   }
+//   // const email = refs.form.elements.email.value;
+//   // const message = refs.form.elements.message.value;
+//   // if (!email || !message) return alert('Все поля должны быть заполнены');
+//   e.currentTarget.reset();
+//   localStorage.removeItem(STORAGE_KEY);
+// }
+
+// function onSavedInput(e) {
+//   if (!localStorage.getItem(STORAGE_KEY)) {
+//     return;
+//   }
+//   const savedInput = JSON.parse(localStorage.getItem(STORAGE_KEY));
+//   if (savedInput) {
+//     refs.email.value = inputValues.email ? inputValues.email : '';
+//     refs.message.value = inputValues.message ? inputValues.message : '';
+//   }
+// }
